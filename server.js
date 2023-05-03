8be54ffdfa2e17aa8099b669310aaead9ee029ce
Lauren <laurenc2335@gmail.com>
2023-05-03 18:49:18 -0500

    My server is set up and running

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const pokemon = require('./models/pokemon');
+
+app.get("/", (req, res) => {
+    res.send(`<h1>'Welcome to the Pokemon App!</h1>`)
+  });
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+  });
