8daa55bdd90535c9610517d557eb3e26f9e5a09d
Lauren <laurenc2335@gmail.com>
2023-05-03 22:39:07 -0500

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -18,9 +18,8 @@ app.get("/", (req, res) => {
   });
 
   app.get('/pokemon', (req, res) => {
-    res.send(pokemon)
+    res.render('Index')
   });
-
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
